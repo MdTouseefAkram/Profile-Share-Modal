455dfc396bbfa553c900b4d0ebcb5091c5af3abb
MdTouseefAkram <mdtouseefakram12769@gmail.com>
2024-05-02 00:40:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+//Modal open function
+const openModal = () => {
+    console.log("Modal is Open");
+    modal.classList.add("active");
+    overlay.classList.add("overlayaractive");
+};
+//Modal close function
+const closeModal = () => {
+    modal.classList.remove("active");
+    overlay.classList.remove("overlayeractive");
+}
